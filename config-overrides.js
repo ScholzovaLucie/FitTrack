9c1f09b8b892758fe07bc401e1616e5d163a2e44
ScholzovaLucie <lucka.scholz@gmail.com>
2024-09-25 07:17:56 +0200

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,13 @@
-module.exports = function override(config, env) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      crypto: require.resolve("crypto-browserify"),
-    };
-    return config;
+module.exports = {
+    webpack: (config) => {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+        vm: require.resolve('vm-browserify'), // Přidáno
+      };
+      return config;
+    },
   };
   
